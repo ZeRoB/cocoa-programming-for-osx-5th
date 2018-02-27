4ec0b6e9d00c34a3e63ade228fc78860ed121ec1
Robert <ZeRoB@users.noreply.github.com>
2018-02-27 17:31:09 +0100

    First Commit

---
+++ 1-RandomPassword/RandomPassword/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  RandomPassword
+//
+//  Created by Robert on 2/27/18.
+//  Copyright © 2018 Robert. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ 1-RandomPassword/RandomPassword/MainWindowController.swift
@@ -0,0 +1,18 @@
+//
+//  MainWindowController.swift
+//  RandomPassword
+//
+//  Created by Robert on 2/27/18.
+//  Copyright © 2018 Robert. All rights reserved.
+//
+
+import Cocoa
+
+class MainWindowController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
