10f1388824f7c4be511b7c6cc4688995ccd13c8a
Robert <ZeRoB@users.noreply.github.com>
2018-03-05 17:33:48 +0100

    1-Add GeneratePassword model and logic

---
+++ 1-RandomPassword/RandomPassword/GeneratePassword.swift
@@ -0,0 +1,31 @@
+//
+//  GeneratePassword.swift
+//  RandomPassword
+//
+//  Created by Robert on 3/5/18.
+//  Copyright © 2018 Robert. All rights reserved.
+//
+
+import Foundation
+
+private let characters = Array("0123456789abcdefghijklmopqrstuvwxyzABCDEFGHIJKMNOPQRSTUVWXYZ")
+
+func generateRandomString(length: Int) -> String {
+    // Start with an empty string
+    var string = ""
+    
+    // Append 'length' number of random characters
+    for _ in 0..<length {
+        string.append(generateRandomCharacter())
+    }
+    
+    return string
+}
+
+func generateRandomCharacter() -> Character {
+    // Create a random index into the characters array
+    let index = Int(arc4random_uniform(UInt32(characters.count)))
+    // Get and return a random character
+    let character = characters[index]
+    return character
+}
