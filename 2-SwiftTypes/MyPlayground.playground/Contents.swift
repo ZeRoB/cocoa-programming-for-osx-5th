84938289f0636231f2955980f5ecfa11ba95c741
Rob <zelhoferr@cip.ifi.lmu.de>
2018-03-05 23:49:46 +0100

    2-Add literals and subscripting

---
+++ 2-SwiftTypes/MyPlayground.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+var str = "Hello, playground"
+str = "Hello, Swift"
+let constrStr = str
+
+var nextYear: Int
+var bodyTemp: Float
+var hasPet: Bool
+var arrayOfInts: [Int]
+var dictionaryOfCapitalsByCountry: [String: String]
+var winningLotteryNumbers: Set<Int>
+
+let number = 42
+let fmStation = 91.1
+let countingUp = ["one", "two"]
+let secondElement = countingUp[1]
+let nameByParkingSpace = [13: "Alice", 27: "Bob"]
