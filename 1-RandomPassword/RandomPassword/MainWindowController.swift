36777f34b021508f69c9a28768021a53c5ba638d
Robert <ZeRoB@users.noreply.github.com>
2018-03-05 17:23:06 +0100

    1-Add outlet and action

--- 1-RandomPassword/RandomPassword/MainWindowController.swift
+++ 1-RandomPassword/RandomPassword/MainWindowController.swift
@@ -10,10 +10,16 @@ import Cocoa
 
 class MainWindowController: NSWindowController {
 
+    @IBOutlet weak var textField: NSTextField!
+    
     override func windowDidLoad() {
         super.windowDidLoad()
 
         // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+    
+    @IBAction func generatePassword(_ sender: Any) {
+        textField.stringValue = "button clicked"
     }
     
 }
