30a49111b14b486d04226beb5c7214c4576166f9
Rob <zelhoferr@cip.ifi.lmu.de>
2018-03-06 11:20:54 +0100

    2-Add Optionals

--- 2-SwiftTypes/MyPlayground.playground/Contents.swift
+++ 2-SwiftTypes/MyPlayground.playground/Contents.swift
@@ -14,7 +14,48 @@ var dictionaryOfCapitalsByCountry: [String: String]
 var winningLotteryNumbers: Set<Int>
 
 let number = 42
+let meaningOfLife = String(number)
 let fmStation = 91.1
-let countingUp = ["one", "two"]
+var countingUp = ["one", "two"]
+countingUp.append("three")
+let countingDown = countingUp.reversed()
 let secondElement = countingUp[1]
 let nameByParkingSpace = [13: "Alice", 27: "Bob"]
+
+let emptyString = String()
+emptyString.isEmpty
+let emptyArray = [Int]()
+let emptySetOfFloats = Set<Float>()
+let defaultNumber = Int()
+let defaultBool = Bool()
+
+let availableRooms = Set([205, 411, 412])
+
+let defaultFloat = Float()
+let floatFromLiteral = Float(3.14)
+
+let easyPi = 3.14
+let floatFromDouble = Float(easyPi)
+let floatingPi: Float = 3.14
+
+var reading1: Float?
+var reading2: Float?
+var reading3: Float?
+reading1 = 9.8
+reading2 = 9.2
+reading3 = 9.7
+if let r1 = reading1, let r2 = reading2, let r3 = reading3 {
+    let avgReading = (r1 + r2 + r3) / 3
+} else {
+    let errorString = "Instrument reported a reading that was nil."
+}
+
+
+
+
+
+
+
+
+
+
