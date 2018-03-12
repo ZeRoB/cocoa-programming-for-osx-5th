d76971a9ded5ad7c906b0bed014b8fa0583a69eb
Robert <ZeRoB@users.noreply.github.com>
2018-03-12 18:09:49 +0100

    5-Creating the MainWindowController class

---
+++ 5-Controls/RGBWell/RGBWell/MainWindowController.swift
@@ -0,0 +1,13 @@
+//
+//  MainWindowController.swift
+//  RGBWell
+//
+//  Created by Robert on 3/12/18.
+//  Copyright © 2018 Robert. All rights reserved.
+//
+
+import Cocoa
+
+class MainWindowController: NSWindowController {
+    
+}
