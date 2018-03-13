2f637e8ad0a4f6015daa58727b5e3cfe20b0d893
Robert <ZeRoB@users.noreply.github.com>
2018-03-13 16:20:00 +0100

    6-Delegation setup

---
+++ 6-Delegation/SpeakLine/SpeakLine/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SpeakLine
+//
+//  Created by Robert on 3/13/18.
+//  Copyright © 2018 Robert. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
