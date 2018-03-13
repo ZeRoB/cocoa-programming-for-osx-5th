ba03ebe16eebbb4984545416067346375f4d58e0
Robert <ZeRoB@users.noreply.github.com>
2018-03-13 16:44:56 +0100

    6-Updating buttons

--- 6-Delegation/SpeakLine/SpeakLine/MainWindowController.swift
+++ 6-Delegation/SpeakLine/SpeakLine/MainWindowController.swift
@@ -15,6 +15,12 @@ class MainWindowController: NSWindowController {
     @IBOutlet weak var stopButton: NSButton!
     
     let speechSynth = NSSpeechSynthesizer()
+    
+    var isStarted: Bool = false {
+        didSet {
+            updateButtons()
+        }
+    }
 
     override var windowNibName: NSNib.Name? {
         return NSNib.Name(rawValue: "MainWindowController")
@@ -22,6 +28,7 @@ class MainWindowController: NSWindowController {
     
     override func windowDidLoad() {
         super.windowDidLoad()
+        updateButtons()
 
         // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
     }
@@ -35,11 +42,23 @@ class MainWindowController: NSWindowController {
             print("string from \(textField) is empty")
         } else {
             speechSynth.startSpeaking(string)
+            isStarted = true
         }
         
     }
     @IBAction func stopIt(_ sender: NSButton) {
         speechSynth.stopSpeaking()
+        isStarted = false
+    }
+    
+    func updateButtons() {
+        if isStarted {
+            speakButton.isEnabled = false
+            stopButton.isEnabled = true
+        } else {
+            stopButton.isEnabled = false
+            speakButton.isEnabled = true
+        }
     }
     
 }
