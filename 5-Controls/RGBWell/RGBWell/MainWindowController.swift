27c652cd157258ea6b329d400fff273941f81d61
Robert <ZeRoB@users.noreply.github.com>
2018-03-13 14:28:56 +0100

    5-Changing the color of the well

--- 5-Controls/RGBWell/RGBWell/MainWindowController.swift
+++ 5-Controls/RGBWell/RGBWell/MainWindowController.swift
@@ -9,6 +9,11 @@
 import Cocoa
 
 class MainWindowController: NSWindowController {
+    var r = 0.0
+    var g = 0.0
+    var b = 0.0
+    var a = 1.0
+    
     @IBOutlet weak var colorWell: NSColorWell!
     
     override var windowNibName: NSNib.Name? {
@@ -16,11 +21,22 @@ class MainWindowController: NSWindowController {
     }
     @IBAction func adjustRed(_ sender: NSSlider) {
         print("R slider's value is \(sender.floatValue)")
+        r = sender.doubleValue
+        updateColor()
     }
     @IBAction func adjustGreen(_ sender: NSSlider) {
         print("G slider's value is \(sender.floatValue)")
+        g = sender.doubleValue
+        updateColor()
     }
     @IBAction func adjustBluw(_ sender: NSSlider) {
         print("B slider's value is \(sender.floatValue)")
+        b = sender.doubleValue
+        updateColor()
+    }
+    
+    func updateColor() {
+        let newColor = NSColor(calibratedRed: CGFloat(r), green: CGFloat(g), blue: CGFloat(b), alpha: CGFloat(a))
+        colorWell.color = newColor
     }
 }
