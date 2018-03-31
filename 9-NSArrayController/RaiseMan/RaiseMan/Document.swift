cc43f463361964389707c4a6a185fd8e56222eb6
Rob <zelhoferr@cip.ifi.lmu.de>
2018-03-31 12:21:09 +0200

    9-Model layer

--- 9-NSArrayController/RaiseMan/RaiseMan/Document.swift
+++ 9-NSArrayController/RaiseMan/RaiseMan/Document.swift
@@ -9,6 +9,8 @@
 import Cocoa
 
 class Document: NSDocument {
+    
+    var employees: [Employee] = []
 
     override init() {
         super.init()
@@ -28,14 +30,13 @@ class Document: NSDocument {
     override func data(ofType typeName: String) throws -> Data {
         // Insert code here to write your document to data of the specified type. If outError != nil, ensure that you create and set an appropriate error when returning nil.
         // You can also choose to override fileWrapperOfType:error:, writeToURL:ofType:error:, or writeToURL:ofType:forSaveOperation:originalContentsURL:error: instead.
-        throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+        return Data()
     }
 
     override func read(from data: Data, ofType typeName: String) throws {
         // Insert code here to read your document from the given data of the specified type. If outError != nil, ensure that you create and set an appropriate error when returning false.
         // You can also choose to override readFromFileWrapper:ofType:error: or readFromURL:ofType:error: instead.
         // If you override either of these, you should also override -isEntireFileLoaded to return false if the contents are lazily loaded.
-        throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
     }
 
 
---
+++ 9-NSArrayController/RaiseMan/RaiseMan/Employee.swift
@@ -0,0 +1,14 @@
+//
+//  Employee.swift
+//  RaiseMan
+//
+//  Created by Rob on 31.03.18.
+//  Copyright © 2018 zero. All rights reserved.
+//
+
+import Foundation
+
+class Employee: NSObject {
+    var name: String? = "New Employee"
+    var raise: Float = 0.05
+}
